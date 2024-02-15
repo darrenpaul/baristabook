d1cb57686b41cba25293d7c89088d48ac6cdd677
Darren Paul <drobertpaul@gmail.com>
2024-02-15 17:46:02 +0100

    console.log

--- utils/array-helpers.ts
+++ utils/array-helpers.ts
@@ -3,6 +3,5 @@ export function findObject(
   key: string,
   matchString: string
 ) {
-  console.log(searchArray, key, matchString);
   return searchArray.find((item) => matchString === item[key]);
 }
